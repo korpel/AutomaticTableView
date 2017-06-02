18a873ba3b00926be49d47ccebe2a33e3adcdf4a
korpel <tony@Antoniss-MacBook-Pro.local>
2017-06-02 21:33:03 +0300

    init

---
+++ AutomaticTableView/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  AutomaticTableView
+//
+//  Created by Antonis Vozikis on 17/05/2017.
+//  Copyright © 2017 Antonis Vozikis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return 1
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")!
+        
+        return cell
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        tableView.estimatedRowHeight = 100
+        tableView.rowHeight = UITableViewAutomaticDimension
+    }
+}
+
